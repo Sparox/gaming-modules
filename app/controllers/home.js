ac7443849aae222823c8cee03ef651ad0c0a04fa
Sparox <lp-57@hotmail.fr>
2016-04-19 22:30:33 +0200

    Update socket.io engine and add redis cache

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,8 +1,17 @@
 var express = require('express'),
-  router = express.Router(),
-  app = express(),
-  server = require('http').createServer(app),
-  io = require('socket.io').listen(server);
+  router = express.Router();
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var pub = require('redis').createClient(6379,'gaming-modules-test.redis.cache.windows.net', {auth_pass: 'fKf+tGjxJxPAhwaq6RxgRQcyu6eSLtS2wEZIpppp0go=', return_buffers: true});
+var sub = require('redis').createClient(6379,'gaming-modules-test.redis.cache.windows.net', {auth_pass: 'fKf+tGjxJxPAhwaq6RxgRQcyu6eSLtS2wEZIpppp0go=', return_buffers: true});
+
+var redis = require('socket.io-redis');
+io.adapter(redis({pubClient: pub, subClient: sub}));
+
+
+var port = process.env.PORT || 3001;
 
 module.exports = function (app) {
   app.use('/', router);
@@ -12,15 +21,71 @@ router.get('/', function (req, res, next) {
     res.render('index', {});
 });
 
-var ioHandler = function (socket) {
-  console.log('Connection to client established');
-  socket.on("chat:message", function(msg){
-    io.emit("chat:message", msg);
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
-};
 
-io.on("connection", ioHandler);
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
 
-server.listen(3001, function(){
-  console.log('listening on *:3001');
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
